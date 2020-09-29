a4896474240abb9bf00eaef352eda275d8c09028
Joe Zou <yixian.zou@gmail.com>
2020-09-29 18:11:53 +0800

    test fail

--- golang/helloworld/dubbo/go-client/app/client.go
+++ golang/helloworld/dubbo/go-client/app/client.go
@@ -55,7 +55,7 @@ func main() {
 	gxlog.CInfo("\n\n\nstart to test dubbo")
 	user := &User{}
 	err := userProvider.GetUser(context.TODO(), []interface{}{"A001"}, user)
-	if err != nil {
+	if err == nil {
 		panic(err)
 	}
 	gxlog.CInfo("response result: %v\n", user)
