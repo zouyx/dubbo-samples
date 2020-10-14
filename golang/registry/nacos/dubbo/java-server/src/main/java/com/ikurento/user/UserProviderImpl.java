19d7790e5e6ac28b99ac46e0e10a57a682c65803
Joe Zou <yixian.zou@gmail.com>
2020-10-14 10:49:49 +0800

    add test case

--- golang/registry/nacos/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
+++ golang/registry/nacos/dubbo/java-server/src/main/java/com/ikurento/user/UserProviderImpl.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UserProviderImpl implements UserProvider {
-    private static final Logger LOG = LoggerFactory.getLogger("UserLogger"); //Output to user-server.log
 
     public User GetUser(String userId) {
         return new User(userId, "zhangsan", 18);
